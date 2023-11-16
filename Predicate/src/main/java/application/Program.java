c30761852948e09496f656485bfcef30b77ca7e6
Kaio Rodrigo <dossantos.krf@gmail.com>
2023-11-16 06:40:07 -0300

    Estudo de predicate, v1 implementação da interface

---
+++ Predicate/src/main/java/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Fazer um programa que, a partir de uma lista de produtos, remova da
+lista somente aqueles cujo preço mínimo seja 100.
+ */
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(new ProductPredicate());
+
+        for (Product p : list){
+            System.out.println(p);
+        }
+    }
+}
