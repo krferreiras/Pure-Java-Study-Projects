f9cda08f635f4af9fece3ade49bbc5edb0ec2b9b
Kaio Rodrigo <dossantos.krf@gmail.com>
2023-11-16 06:45:10 -0300

    Estudo predicate, v2 Reference Method

--- Predicate/src/main/java/entities/Product.java
+++ Predicate/src/main/java/entities/Product.java
@@ -26,6 +26,10 @@ public class Product {
         this.price = price;
     }
 
+    public static boolean staticProductPredicate(Product product){
+        return product.getPrice() >= 100.0;
+    }
+
     @Override
     public String toString() {
         return name +
