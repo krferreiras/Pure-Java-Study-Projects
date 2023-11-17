c7b65d766702dc2410856f020e3d7cb84902f3ff
Kaio Rodrigo <dossantos.krf@gmail.com>
2023-11-17 10:20:40 -0300

    Estudo de function, v2 com reference method com método estático

--- Function/src/main/java/entities/Product.java
+++ Function/src/main/java/entities/Product.java
@@ -26,6 +26,10 @@ public class Product {
         this.price = price;
     }
 
+    public static String staticUpperCaseName(Product product){
+        return product.getName().toUpperCase();
+    }
+
     @Override
     public String toString() {
         return name +
